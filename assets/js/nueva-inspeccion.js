f5b4b20371b4fc3d266f9f3461a5457ae06b909f
yohane45 <yohaneadid@gmail.com>
2026-08-20 21:04:36 -0500

    formulario 1
    tratar de que salgan mensajes establecidos después de dar click en el botón guardar, todavía en correcciones

--- assets/js/nueva-inspeccion.js
+++ assets/js/nueva-inspeccion.js
@@ -2,3 +2,25 @@ const botonCancelar = document.querySelector("#cancelar-inspeccion");
     botonCancelar.addEventListener("click", function(){
     window.location.href = "inspecciones.html";
 });
+const formulario = document.querySelector("#form-nueva-inspeccion");
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault();
+    const tipo = document.querySelector("#tipo-inspeccion").value;
+    const area = document.querySelector("#area-inspeccion").value;
+    const fecha = document.querySelector("#fecha-inspeccion").value;
+    const responsable = document.querySelector("#responsable-inspeccion").value;
+    const objetivo = document.querySelector("#objetivo-inspeccion").value;
+
+    if (
+        tipo === "" ||
+        area === "" ||
+        fecha === "" ||
+        responsable.trim() === "" ||
+        objetivo.trim() === ""
+
+    ) {
+        alert("Por favor, complete los campos obligatorios.");
+        return;
+    }
+    alert("La inspeccion fue validada correctamente.");
+});
