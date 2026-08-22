7bb3bee7b3866b21d684bde9b231aa12fcff11b6
yohane45 <yohaneadid@gmail.com>
2026-08-21 20:48:08 -0500

    Bloque de guardado funcional
    se agrega el bloque de guardado de una nueva inspección

--- assets/js/nueva-inspeccion.js
+++ assets/js/nueva-inspeccion.js
@@ -26,5 +26,25 @@ formularioInspeccion.addEventListener("submit", function(event){
         alert("Por favor, complete los campos obligatorios.");
         return;
     }
-    alert("La inspeccion fue validada correctamente.");
+    // bloque de guardado //
+    const inspeccionesGuardadas = 
+        JSON.parse(localStorage.getItem("inspecciones")) || [];
+    const nuevoId = `IN-${String(inspeccionesGuardadas.length + 4).padStart(3, "0")}`;
+
+    const inspecciones = {
+        id: nuevoId,
+        fecha: fecha,
+        tipo: tipo,
+        area: area,
+        responsable: responsable,
+        estado: "Pendiente",
+        objetivo: objetivo
+    };
+    inspeccionesGuardadas.push(inspecciones);
+    localStorage.setItem(
+        "inspecciones",
+        JSON.stringify(inspeccionesGuardadas)
+    );
+    alert("La inspeccion fue guardada correctamente.");
+    window.location.href = "inspecciones.html";
 });
