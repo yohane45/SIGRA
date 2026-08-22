89700effea16e3954a818a9f6b051feaee30cc61
yohane45 <yohaneadid@gmail.com>
2026-08-21 20:49:14 -0500

    Bloque de inspecciones guardadas
    Se agrega este bloque para poder visualizar la inspección guardada en la tabla.

--- assets/js/inspecciones.js
+++ assets/js/inspecciones.js
@@ -18,3 +18,34 @@ modalMarcoNormativo.addEventListener("click", function(evento){
         modalMarcoNormativo.classList.remove("active");
     }
 });
+// Cargar inspecciones guardadas
+const inspeccionesGuardadas = 
+    JSON.parse(localStorage.getItem("inspecciones")) || [];
+const tablaInspecciones = 
+    document.querySelector("#tabla-inspecciones tbody");
+inspeccionesGuardadas.forEach(function(inspecciones){
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+        <td>${inspecciones.id}</td>
+        <td>${inspecciones.fecha}</td>
+        <td>${inspecciones.tipo}</td>
+        <td>${inspecciones.area}</td>
+        <td>${inspecciones.responsable}</td>
+        <td>
+            <span class="status-danger">${inspecciones.estado}</span>
+        </td>
+        <td class="actions">
+            <button class="icon-button" title="Ver">
+                <i class="bi bi-eye"></i>
+            </button>
+            <button class="icon-button" title="Editar">
+                <i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="icon-button" title="Eliminar">
+                <i class="bi bi-trash"></i>
+                </button>
+            </td>
+            `;
+            tablaInspecciones.appendChild(fila);
+});        
+
