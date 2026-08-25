df11b69eb3c61d6bf3c8925b26f9109a2771e7ce
yohane45 <yohaneadid@gmail.com>
2026-08-25 10:23:49 -0500

    marco normativo
    agrego funciones al botón

--- assets/js/incidentes.js
+++ assets/js/incidentes.js
@@ -2,5 +2,26 @@ console.log("incidentes.js cargado");
 const botonNuevoIncidente = document.querySelector("#nuevo-incidente");
 botonNuevoIncidente.addEventListener("click", function(){
     window.location.href = "nuevo-incidente.html";
+});
 
+// Marco Normativo
+const botonMarcoNormativo =
+    document.querySelector("#marco-normativo");
+
+const modalMarcoNormativo = 
+    document.querySelector("#modal-marco-normativo");
+    
+const cerrarMarcoNormativo =
+    document.querySelector("#cerrar-marco-normativo");
+
+botonMarcoNormativo.addEventListener("click", function(){
+    modalMarcoNormativo.classList.add("active");
+});
+cerrarMarcoNormativo.addEventListener("click", function (){
+    modalMarcoNormativo.classList.remove("active");
+});
+modalMarcoNormativo.addEventListener("click", function (event){
+    if (event.target === modalMarcoNormativo) {
+        modalMarcoNormativo.classList.remove("active");
+    }
 });
