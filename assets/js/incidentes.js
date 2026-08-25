5e782e71860b3e6d11eead7f26584149ced16fe4
yohane45 <yohaneadid@gmail.com>
2026-08-25 09:23:35 -0500

    botón cancelar
    se agrega función al botón cancelar y al botón de nuevo incidente

---
+++ assets/js/incidentes.js
@@ -0,0 +1,6 @@
+console.log("incidentes.js cargado");
+const botonNuevoIncidente = document.querySelector("#nuevo-incidente");
+botonNuevoIncidente.addEventListener("click", function(){
+    window.location.href = "nuevo-incidente.html";
+
+});
---
+++ assets/js/nuevo-incidente.js
@@ -0,0 +1,6 @@
+console.log("nuevo-incidentes.js cargado");
+const botonNuevoIncidente = document.querySelector("#cancelar-incidente");
+botonNuevoIncidente.addEventListener("click", function(){
+    window.location.href = "incidentes.html";
+
+});
