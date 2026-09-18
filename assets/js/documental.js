2075e2c6b76f850b092468163d7f994d4b58cc66
yohane45 <yohaneadid@gmail.com>
2026-09-18 11:57:22 -0500

    Nuevo-documento
    funcional

--- assets/js/documental.js
+++ assets/js/documental.js
@@ -7,3 +7,30 @@ const nuevoDocumento = document.getElementById("nuevo-documento");
 nuevoDocumento.addEventListener("click", function () {
     window.location.href = "nuevo-documento.html";
 });
+
+// =========================================
+// MARCO NORMATIVO
+// =========================================
+
+const botonMarcoNormativo = document.getElementById("marco-normativo");
+const modalMarcoNormativo = document.getElementById("modal-marco-normativo");
+const cerrarMarcoNormativo = document.getElementById("cerrar-marco-normativo");
+const cerrarMarcoNormativoFooter = document.getElementById("cerrar-marco-normativo-footer");
+
+botonMarcoNormativo.addEventListener("click", function(){
+    modalMarcoNormativo.classList.add("active");
+});
+
+cerrarMarcoNormativo.addEventListener("click", function(){
+    modalMarcoNormativo.classList.remove("active");
+});
+
+cerrarMarcoNormativoFooter.addEventListener("click", function(){
+    modalMarcoNormativo.classList.remove("active");
+});
+
+modalMarcoNormativo.addEventListener("click", function (evento){
+    if (evento.target === modalMarcoNormativo){
+        modalMarcoNormativo.classList.remove("active");
+    }
+});
